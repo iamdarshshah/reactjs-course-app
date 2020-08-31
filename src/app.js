fdff7008e76e3970a9df69ddcb387d444e746bff
iamdarshshah <imdarshshah@gmail.com>
2020-08-31 22:42:51 +0530

    💪 You did it! You just made your first ReactJS app

---
+++ src/app.js
@@ -0,0 +1,42 @@
+// @ts-nocheck
+
+// Rendering static data
+const FullNameStatic = () =>
+  React.createElement("div", {}, [
+    React.createElement("h2", {}, "Darsh"),
+    React.createElement("h3", {}, "Shah"),
+  ]);
+
+// Passing props to the child component
+const FullNameWithProps = (props) =>
+  React.createElement("div", {}, [
+    React.createElement("h2", {}, props.firstName),
+    React.createElement("h3", {}, props.lastName),
+  ]);
+
+// Using Destructuring
+const FullNameWithoutProps = ({ firstName, lastName }) =>
+  React.createElement("div", {}, [
+    React.createElement("h2", {}, firstName),
+    React.createElement("h3", {}, lastName),
+  ]);
+
+const App = () =>
+  React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, [
+      "Hello Everyone👋",
+      React.createElement(FullNameStatic),
+      React.createElement(FullNameWithProps, {
+        firstName: "Yash",
+        lastName: "Adulkar",
+      }),
+      React.createElement(FullNameWithoutProps, {
+        firstName: "Brian",
+        lastName: "holt",
+      }),
+    ])
+  );
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
